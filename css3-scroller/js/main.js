b49fbd0a18f2bf4f50bbab8dcdf43a9b5816de56
보롱킴 <dsdgun@gmail.com>
2014-06-27 00:53:30 +0900

    jQuery 기반 코드로 변환

--- css3-scroller/js/main.js
+++ css3-scroller/js/main.js
@@ -1,19 +1,21 @@
-
-function randomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-window.onload = function() {
-	var appendTarget = document.body;
-	var card;
-	for(var i=0;i<1000;i++) {
-		card = document.createElement("div");
-		card.setAttribute("class","card");
-		appendTarget.appendChild(card);
-		card.style.backgroundColor = randomColor();
+$(function() {
+	function randomColor() {
+	    var t0 = '0123456789ABCDEF'.split('');
+	    var t1 = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        t1 += t0[Math.floor(Math.random() * 16)];
+	    }
+	    return t1;
 	}
-};
+	var $body = $("body");
+	for(var i=0;i<1000;i++) {
+		$("<div />").attr({	"class":"card", "id":"card" + i
+			})
+			.css("background-color", randomColor())
+			.appendTo("body")
+			.bind("click", function(e){
+				e.stopPropagation();
+				console.log($(this));
+			});
+	}
+});
