5834cb6d5bb2bf97647a9c7371d9c2488d3a437f
Bohyung Kim <dsdgun@gmail.com>
2018-07-31 16:54:04 +0900

    recognize window resize

--- kdtree/main.js
+++ kdtree/main.js
@@ -4,8 +4,6 @@ const canvas = (() => {
   return c
 })()
 const context = canvas.getContext('2d')
-context.strokeStyle = '#fff'
-context.lineWidth = 1
 
 let data = {
   strokes: {}
@@ -15,8 +13,14 @@ const normalize = (x) => x + 0.5
 canvas.addEventListener('mousedown', ({clientX, clientY}) => {
   let stroke = {color:'#fff', lineWidth: 1, points: [], start:null, end: null, id: Date.now()}
   var previousPoint = {x: clientX, y:clientY}
-  context.moveTo(normalize(previousPoint.x), normalize(previousPoint.y))
+  const w = canvas.width, h = canvas.height
   stroke.start = previousPoint
+  data.strokes[stroke.id] = stroke
+  context.save()
+  context.strokeStyle = stroke.color
+  context.lineWidth = stroke.lineWidth
+  context.moveTo(normalize(previousPoint.x), normalize(previousPoint.y))
+  
   const mouseDownHandler = ({clientX, clientY}) => {
     let p = {x: clientX, y: clientY}
     stroke.points.push(p)
@@ -26,11 +30,31 @@ canvas.addEventListener('mousedown', ({clientX, clientY}) => {
 
   const mouseUpHandler = ({clientX, clientY}) => {
     stroke.end = {x: clientX, y: clientY}
-    data.strokes[stroke.id] = stroke
     canvas.removeEventListener('mousemove', mouseDownHandler)
     canvas.removeEventListener('mouseup', mouseUpHandler)
     console.log(data)
   }
   canvas.addEventListener('mouseup', mouseUpHandler)
   canvas.addEventListener('mousemove', mouseDownHandler)
+})
+
+function render(strokes) {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  for (let k in strokes) {
+    context.save()
+    context.strokeStyle = strokes[k].color
+    context.lineWidth = strokes[k].lineWidth
+    let previousPoint = strokes[k].start
+    context.moveTo(normalize(previousPoint.x), normalize(previousPoint.y))
+    strokes[k].points.forEach((p) => {
+      context.lineTo(normalize(p.x), normalize(p.y))
+    })
+    context.stroke()
+    context.restore()
+  }
+}
+
+window.addEventListener('resize', () => {
+  canvas.width = canvas.clientWidth, canvas.height = canvas.clientHeight
+  render(data.strokes)
 })
