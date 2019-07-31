0c4e1f0c64e4056ddd21be0cbc0181e863cd5364
Bohyung Kim <dsdgun@gmail.com>
2019-07-31 12:37:17 +0900

    success logging

--- experimentals/qrcode-scanner/index.js
+++ experimentals/qrcode-scanner/index.js
@@ -1,12 +1,13 @@
 (() => {
   console.log(navigator.mediaDevices)
   const errorEl = document.querySelector('#error-msg')
-  const video = document.querySelector('video')
+  const video = document.querySelector('#video')
   const deviceOpts = {
     audio:false,
     video: {facingMode: {exact: 'environment' }}
   }
   navigator.mediaDevices.getUserMedia(deviceOpts).then(stream => {
+    alert('success')
     const videoTracks = stream.getVideoTracks()
 
     stream.onremovetrack = () => console.log('stream ended')
