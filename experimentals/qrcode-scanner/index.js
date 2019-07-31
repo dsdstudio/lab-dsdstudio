1acbe6660e3fc3e1efa088b7c38e9643a2f9091f
Bohyung Kim <dsdgun@gmail.com>
2019-07-31 12:49:42 +0900

    [hotfix] add framerate

--- experimentals/qrcode-scanner/index.js
+++ experimentals/qrcode-scanner/index.js
@@ -1,12 +1,15 @@
 (() => {
   const errorEl = document.querySelector('#error-msg')
-  const video = document.querySelector('#video')
   const deviceOpts = {
     audio:false,
-    video: {facingMode: {exact: 'environment' }}
+    video: {
+      frameRate: { ideal: 10, max: 30 },
+      facingMode: { exact: 'environment' }
+    }
   }
   navigator.mediaDevices.getUserMedia(deviceOpts).then(stream => {
     errorEl.textContent = errorEl.textContent + '\nsuccess' 
+    const video = document.querySelector('video')
     const videoTracks = stream.getVideoTracks()
 
     stream.onremovetrack = () => console.log('stream ended')
