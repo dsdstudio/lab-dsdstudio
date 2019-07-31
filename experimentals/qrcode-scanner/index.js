c1be52505ecb908dfa6b4bdbe61d65e4c6ec24ef
Bohyung Kim <dsdgun@gmail.com>
2019-07-31 13:15:17 +0900

    update constraint

--- experimentals/qrcode-scanner/index.js
+++ experimentals/qrcode-scanner/index.js
@@ -6,16 +6,11 @@
   const errorEl = document.querySelector('#error-msg')
   const deviceOpts = {
     audio:false,
-    video: {
-      width: 1280,
-      height: 720,
-      frameRate: { ideal: 10, max: 30 },
-      facingMode: { exact: 'environment' }
-    }
+    video:true
   }
 
   checkCameraSupport()
-  navigator.mediaDevices.getUserMedia(deviceOpts).then(stream => {
+  navigator.getUserMedia(deviceOpts).then(stream => {
     log('success')
     const video = document.querySelector('video')
     const videoTracks = stream.getVideoTracks()
